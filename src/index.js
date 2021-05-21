374547ddb7667e5c94b940d2168d09a9521afc3d
Arthur V Fain <arthurvfain@gmail.com>
2021-05-21 17:00:47 -0400

    huge buttons

--- src/index.js
+++ src/index.js
@@ -5,10 +5,60 @@ document.querySelector('#create-task-form').addEventListener('submit',e =>
   //console.log(e)
   let toDoElement =
   {
-    newTaskDescription: e.target[0].value
+    newTaskDescription: e.target['new-task-description'].value
   }
-  return toDoElement
+  appendToDoOnDOM(toDoElement);
   //console.log(toDoElement.newTaskDescription)
 })
-console.log(toDoElement.newTaskDescription)
-//document.querySelector('#tasks').appendChild(toDoElement.newTaskDescription.value)
+
+function appendToDoOnDOM(toDoObj)
+{
+  let li = document.createElement('li')
+  li.textContent = toDoObj.newTaskDescription
+  let deleteButton = document.createElement('button')
+  deleteButton.textContent = 'X'
+  deleteButton.style.background='#f00'
+  deleteButton.style.width = '200px'
+  deleteButton.style.height = '200px'
+  deleteButton.style.fontSize = '180px'
+  deleteButton.addEventListener("click", (e) => e.target.parentElement.remove())
+  li.appendChild(deleteButton)
+  document.querySelector('#tasks').appendChild(li)
+  
+}
+
+
+// // Grab Elements From the DOM
+// let taskForm = document.querySelector('#create-task-form')
+
+
+// // Add Event Listeners to DOM Elements
+// taskForm.addEventListener('submit', grabFormValues)
+
+
+// // Manipulate  Data
+// function grabFormValues(e){
+//   e.preventDefault();
+//   let toDoElement =
+//   {
+//     newTaskDescription: e.target['new-task-description'].value
+//   }
+
+//   appendToDoOnDOM(toDoElement);
+// }
+
+// // Append/Add Back to DOM
+// function appendToDoOnDOM(toDoObj)
+// {
+//   let li = document.createElement('li')
+//   li.textContent = toDoObj.newTaskDescription
+
+//   let deleteButton = document.createElement('button')
+//   deleteButton.textContent = 'X'
+
+//   deleteButton.addEventListener("click", (e) => e.target.parentElement.remove())
+  
+//   li.appendChild(deleteButton)
+//   document.querySelector('#tasks').appendChild(li)
+  
+// }
