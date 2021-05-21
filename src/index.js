8bf679c0743d49a1cb82648cfa95f2df12dad671
Arthur V Fain <arthurvfain@gmail.com>
2021-05-21 16:12:21 -0400

    passing to Dan

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", () => {});
+document.querySelector('#create-task-form').addEventListener('submit',e =>
+{
+  e.preventDefault();
+  //console.log(e)
+  let toDoElement =
+  {
+    newTaskDescription: e.target[0].value
+  }
+  return toDoElement
+  //console.log(toDoElement.newTaskDescription)
+})
+console.log(toDoElement.newTaskDescription)
+//document.querySelector('#tasks').appendChild(toDoElement.newTaskDescription.value)
